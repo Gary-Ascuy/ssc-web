c539b96e48dcfc09e4320b4122b9e2d1e72f6593
Gary Ascuy <gary.ascuy@jalasoft.com>
2016-07-24 18:32:23 -0400

    Adding UI events and send commands

--- server/public/client/console.js
+++ server/public/client/console.js
@@ -1,20 +1,41 @@
 window.Client = window.Client || {};
+window.Client.io = io.connect();
 
 //
 // Interactive console
 //
 Client.MainConsole = React.createClass({
+  getInitialState() {
+    return {command: ''};
+  },
+  handleCommandChange: function(e) {
+    this.setState({command: e.target.value});
+  },
+  handleSubmit(evt) {
+    evt.preventDefault();
+    this.sendCommand();
+  },
+  sendCommand() {
+    const {command} = this.state;
+    console.log(this.state);
+    this.setState({command: ''});
+    window.Client.io.send(command);
+  },
   render() {
     return (
       <div className="ui basic segment">
         <div className="main console">
-          <form className="ui form">
+          <form className="ui form" onSubmit={this.handleSubmit}>
             <div className="ui message">
               <p>Execution results</p>
             </div>
             <div className="ui icon input">
-              <input type="text" placeholder="Command"/>
-              <i className="play link icon"></i>
+              <input type="text"
+                value={this.state.command}
+                onChange={this.handleCommandChange}
+                autocomplete="off"
+                placeholder="Command"/>
+              <i className="play link icon" onClick={this.handleSubmit}></i>
             </div>
           </form>
         </div>
