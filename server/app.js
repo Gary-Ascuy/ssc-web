20251c1012b12922b9c0c12a7cb1ea5eb467659f
Gary Ascuy <gary.ascuy@jalasoft.com>
2016-07-24 18:01:19 -0400

    Adding socket.io

--- server/app.js
+++ server/app.js
@@ -1,12 +1,15 @@
 const path = require('path');
 const Koa = require('koa');
 const serve = require('koa-static');
+const IO = require('koa-socket');
 const convert = require('koa-convert');
 
 //
 // Web Application
 //
 const app = new Koa();
+const io = new IO();
+
 const publicFiles = serve(path.join(__dirname, 'public'));
 publicFiles._name = 'static /public';
 app.use(convert(publicFiles));
@@ -14,7 +17,7 @@ app.use(convert(publicFiles));
 //
 // x-response-time
 //
-app.use(async function (ctx, next) {
+app.use(async (ctx, next) => {
   const start = new Date();
   await next();
   const ms = new Date() - start;
@@ -24,7 +27,7 @@ app.use(async function (ctx, next) {
 //
 // Logger
 //
-app.use(async function (ctx, next) {
+app.use(async (ctx, next) => {
   const start = new Date();
   await next();
   const ms = new Date() - start;
@@ -36,10 +39,15 @@ app.use(ctx => {
   ctx.body = '<center><h1>Powered By Gary Ascuy {gary.ascuy@gmail.com}</h1></center>';
 });
 
+io.on( 'message', (ctx, data) => {
+  console.log(`message: ${ data }`);
+});
+
 //
 // HTTP/HTTPS Serve
 //
-const port = process.env.PORT || 3666
+const port = process.env.PORT || 3666;
+io.attach(app);
 app.listen(port);
 console.log(`Koa Server listening at port ${port}`);
 console.log('Serial Servo Controller Web Application - Press ctrl+c to exit.');
