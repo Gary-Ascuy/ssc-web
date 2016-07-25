da09c05cb7a95df6cd4c565c0f173047145e63e5
Gary Ascuy <gary.ascuy@jalasoft.com>
2016-07-24 20:14:32 -0400

    Adding messages

--- agent/index.js
+++ agent/index.js
@@ -3,9 +3,15 @@
 //
 // Serial Servo Controller - Agent
 //
-var socket = require('socket.io-client')('http://localhost:3666');
-socket.on('connect', () => {});
-socket.on('disconnect', () => {});
+const host = process.env.HOST || 'http://localhost:3666'
+var socket = require('socket.io-client')(host);
+socket.on('connect', () => {
+  console.log(`Connected - Agent listening at ${host}`);
+  console.log('Serial Servo Controller Agent - Press ctrl+c to exit.');
+});
+socket.on('disconnect', () => {
+  console.log('Oops !!! There is an Error.')
+});
 
 socket.on('command', (data) => {
   console.log('command', data);
